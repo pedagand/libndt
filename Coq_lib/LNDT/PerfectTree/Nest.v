e0d3b3af61d305e8de4b0b979e6a474ddf24f85f
Amélie <amelie.ledein@orange.fr>
2022-03-25 11:25:00 +0100

    Fix nest example

--- Coq_lib/LNDT/PerfectTree/Nest.v
+++ Coq_lib/LNDT/PerfectTree/Nest.v
@@ -3,6 +3,7 @@
 Require Import Spreadable.
 Require Import LNDT.
 Require Import PerfectTree.
+Require Import Lia.
 
 Definition Nest : TT := PerfectTree 1.
 
@@ -26,34 +27,45 @@ Definition nest_empty       := pt_empty       1.
 Definition nest_dec_in_prop := pt_dec_in_prop 1.
 Definition nest_dec_eq      := pt_dec_eq      1.
 
+Require Import Tuple.
+
 (** ** Example of inhabitant of Nest nat *)
+
 Definition example : Nest nat :=
-  nest _ _ 7 (nest _ _ (1, 2)
-    (nest _ _ ((6, 7), 7, 4)
-       (nest _ _ (((2, 5), 7, 1), (3, 8), (9, 3)) (empty _ _)))).
+  nest _ _ 7 
+   (nest _ _ (1, 2)
+     (nest _ _ ((6, 7), (7, 4))
+       (nest _ _ (((2, 5), (7, 1)), ((3, 8), (9, 3)))
+         (empty _ _)))).
 (* equivalent to
-    7 ∷ (1 , 2) ∷ ((6 , 7) , 7 , 4)
-       ∷ ((2 , 5) , 7 , 1) , (3 , 8) , (9 , 3) ∷ [] *)
+    7 ∷ (1 , 2) ∷ ((6 , 7) , (7 , 4))
+       ∷ (((2 , 5) , (7 , 1)) , ((3 , 8) , (9 , 3))) ∷ [] *)
 
 (** ** Examples of function calls over this example *)
 
 Definition exampleMap : Nest nat :=
-  nest_map _ (fun x => x + 3) example.
+  nest_map _ _ (fun x => x + 3) example.
+
+Eval compute in exampleMap.
 
 Definition exampleFoldr : nat :=
-  nest_foldr _ (fun x y => x + y) 0 example.
+  nest_foldr _ _ (fun x y => x + y) 0 example.
+
+Eval compute in exampleFoldr.
 
 Definition exampleFoldl : nat :=
-  nest_foldl _ (fun x y => x + y) 0 example.
+  nest_foldl _ _ (fun x y => x + y) 0 example.
 
-Definition exampleSize : nat := nest_size example.
+Eval compute in exampleFoldl.
+
+Definition exampleSize : nat := nest_size _ example.
 
 Eval compute in exampleSize.
 
 (** ** Examples of predications over these examples *)
 
 Lemma exampleAny : nest_any _ (fun x => x > 8) example.
-Proof. Defined.
+Proof. unfold nest_any, example, pt_any. simpl. lia. Defined.
 
-Lemma exampleAll : nest_all _ (fun x => X >= 1) example.
-Proof. Defined.
+Lemma exampleAll : nest_all _ (fun x => x >= 1) example.
+Proof. unfold nest_all, example, pt_all. simpl. repeat split;lia. Defined.
